54dd182848e9c4b5fd663823705c41c32ad25d8e
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2020-01-19 00:16:11 +0100

    Autostart Skript

--- start-last-wss-player.sh
+++ start-last-wss-player.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 #get command to start the last used player
-. ./last-player
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+. "${DIR}/last-player"
 echo "${AUTOSTART}"
