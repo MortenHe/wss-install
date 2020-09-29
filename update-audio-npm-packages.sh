618cd474a1a45c4d5aa3034e30faff6388aff1a7
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2020-09-29 11:01:15 +0200

    npm install statt update

--- update-audio-npm-packages.sh
+++ update-audio-npm-packages.sh
@@ -4,32 +4,32 @@
 . $(dirname "$0")/config
 
 #Audio Server NPM packages updaten
-echo 'update AudioServer npm packages'
-npm update --prefix /home/pi/mh_prog/AudioServer
+echo 'install AudioServer npm packages'
+npm install --prefix /home/pi/mh_prog/AudioServer
 echo
 
 #SH Audio Server NPM packages updaten
-echo 'update NewSHAudioServer npm packages'
-npm update --prefix /home/pi/mh_prog/NewSHAudioServer
+echo 'install NewSHAudioServer npm packages'
+npm install --prefix /home/pi/mh_prog/NewSHAudioServer
 echo
 
 #SoundQuiz Server NPM packages updaten
-echo 'update SoundQuizServer npm packages'
-npm update --prefix /home/pi/mh_prog/SoundQuizServer
+echo 'install SoundQuizServer npm packages'
+npm install --prefix /home/pi/mh_prog/SoundQuizServer
 echo
 
 #GPIO Buttons NPM packages updaten
 if [ $GPIOBUTTONS = true ];
 then
-  echo 'update WSGpioButtons npm packages'
-  npm update --prefix /home/pi/mh_prog/WSGpioButtons
+  echo 'install WSGpioButtons npm packages'
+  npm install --prefix /home/pi/mh_prog/WSGpioButtons
   echo
 fi
 
 #USB RFID Reader NPM packages updaten
 if [ $USBRFIDREADER = true ];
 then
-  echo 'update WSRFID npm packages'
-  npm update --prefix /home/pi/mh_prog/WSRFID
+  echo 'install WSRFID npm packages'
+  npm install --prefix /home/pi/mh_prog/WSRFID
   echo
 fi
