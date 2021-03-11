344c2168c5f58aedb65c7754cb383dfe8f49e179
Martin Helfer <martin-helfer@gmx.de>
2021-03-11 22:57:24 +0100

    STT auch bei Stille am Ende (!)

--- stt-mh.py
+++ stt-mh.py
@@ -9,13 +9,13 @@ import json
 SetLogLevel(-1)
 
 if not os.path.exists("model"):
-    print ("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
-    exit (1)
+    print("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+    exit(1)
 
 wf = wave.open(sys.argv[1], "rb")
 if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
-    print ("Audio file must be WAV format mono PCM.")
-    exit (1)
+    print("Audio file must be WAV format mono PCM.")
+    exit(1)
 
 model = Model("model")
 rec = KaldiRecognizer(model, wf.getframerate())
@@ -25,7 +25,8 @@ while True:
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        (rec.Result())
+        pass
+        # (rec.Result())
     else:
         (rec.PartialResult())
 
