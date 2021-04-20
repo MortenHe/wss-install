6f36029e360259360503d6bf90e22c95f3a8f891
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2021-04-20 21:59:30 +0200

    config

--- install-iqaudio.sh
+++ install-iqaudio.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#TODO: 
+#Add the following line to /boot/config.txt and reboot:
+#dtoverlay=i2s-mmap
+#copy /etc/asound
+#TODO: alsamixer Gain -6dB
+
 #Iqaudio Card
 echo 'set IQAUDIO card'
 sed 's/dtparam=audio=on/dtoverlay=iqaudio-dacplus/' -i /boot/config.txt
